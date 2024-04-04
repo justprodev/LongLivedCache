dd099b37343e144bb55e2f02cf14aad52aa529ce
Alexander Yemelyanov <alex@justprodev.com>
2024-04-04 17:34:28 +0500

    simplify invalidator

--- src/main/java/com/justprodev/cache/CacheInvalidator.kt
+++ src/main/java/com/justprodev/cache/CacheInvalidator.kt
@@ -1,101 +1,42 @@
 package com.justprodev.cache
 
-import kotlinx.coroutines.*
+import com.justprodev.cache.update.ScheduledUpdate
+import com.justprodev.cache.update.Scheduler
 import org.slf4j.Logger
-import java.util.*
-import java.util.concurrent.Executors
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.timerTask
 
 /**
  * Shadow updating the cache entities in parallel
  *
- * [invalidatorDelay] - delay before invalidating cache entities, to group them
+ * @param period - period between updating scheduled agents
+ * @param maxThreads - maximum threads for parallel updating cache entities
+ * @param logger - logger for debug information
  */
 internal class CacheInvalidator(
-    private val invalidatorDelay: Long,
+    period: Long,
     maxThreads: Int,
-    private val logger: Logger
+    logger: Logger,
 ) {
-    private val agentsDispatcher = Executors.newFixedThreadPool(maxThreads).asCoroutineDispatcher()
-    private val lock = ReentrantLock()
-    private val timer = Timer()
-    private var task: TimerTask? = null
-    private val agents = HashMap<String, AgentToInvalidate>()
-
-    private class AgentToInvalidate(val agent: CacheAgent<*>, val onFinish: (() -> Unit)? = null)
+    private val scheduler = Scheduler(period, maxThreads, logger)
 
     /**
-     * Invalidates agent and its roots
+     * Schedules an updating of the [agent]
+     *
+     * Also, schedules updating of all roots of the [agent] recursively
+     *
+     * @param agent to update
+     * @param onFinish optional callback that will be called after updating
      */
-    fun invalidate(agent: CacheAgent<*>, onFinish: (() -> Unit)? = null) = with(agent) {
-        logger.debug("invalidate '$name'")
-        schedule(this, onFinish)
-    }
-
-    private fun schedule(agent: CacheAgent<*>, onFinish: (() -> Unit)?) {
-        lock.lock()
-        addAgent(agent, onFinish)
-        if (task == null) {
-            task = createTask()
-            timer.schedule(task, invalidatorDelay)
-        }
-        lock.unlock()
-    }
-
-    private fun createTask() = timerTask {
-        lock.lock()
-        val _agents = agents.values.toList()
-        agents.clear()
-        task = null
-        lock.unlock()
-
-        // here We have graph
-        //          0
-        //       1     1
-        //    2     2     2
-        // We can separate levels 0, 1, 2 and run its in parallel
-        // But, if we are at level 1, we must wait for the data to update at level 2
-        val levelSortedAgents = _agents.sortedByDescending { it.agent.level }
-        var level = levelSortedAgents.firstOrNull()?.agent?.level
-
-        class CacheAgentJob(val job: Job, val agent: CacheAgent<*>)
-
-        val levelJobs = mutableListOf<CacheAgentJob>()
-
-        runBlocking {
-            levelSortedAgents.forEach { agentToInvalidate ->
-                val agent = agentToInvalidate.agent
-                val onFinish = agentToInvalidate.onFinish
-
-                with(agent) {
-                    if (level != this.level) {
-                        // We should get data updated at the nested level before starting update data at this level
-                        // I.e. - wait previous jobs to complete
-                        levelJobs.forEach {
-                            logger.debug("wait update '${it.agent.name}' (level ${it.agent.level})")
-                            it.job.join()
-                        }
-                        level = this.level
-                        levelJobs.clear()
-                    }
-                    logger.debug("schedule update '$name' (level ${this.level})")
-                    val job = GlobalScope.launch(agentsDispatcher) {
-                        update()
-                        logger.debug("updated '$name' (level ${this@with.level})")
-                        onFinish?.invoke()
-                    }
-                    levelJobs.add(CacheAgentJob(job, this))
-                }
+    fun invalidate(agent: CacheAgent<*>, onFinish: (() -> Unit)? = null) {
+        val updates = arrayListOf<ScheduledUpdate>().apply {
+            // add agent and all roots recursively
+            fun addAgent(agent: CacheAgent<*>, onFinish: (() -> Unit)? = null) {
+                add(ScheduledUpdate(agent, onFinish))
+                agent.roots?.forEach(::addAgent)
             }
-        }
-    }
 
-    // add agent and all roots recursively
-    private fun addAgent(agent: CacheAgent<*>, onFinish: (() -> Unit)? = null) {
-        agents[agent.name] = AgentToInvalidate(agent, onFinish)
-        agent.roots?.forEach {
-            addAgent(it)
+            addAgent(agent, onFinish)
         }
+        scheduler.add(updates)
     }
 }
+
---
+++ src/main/java/com/justprodev/cache/update/Scheduler.kt
@@ -0,0 +1,132 @@
+package com.justprodev.cache.update
+
+import com.justprodev.cache.CacheAgent
+import kotlinx.coroutines.*
+import org.slf4j.Logger
+import java.util.*
+import java.util.concurrent.Executors
+import kotlin.concurrent.scheduleAtFixedRate
+
+/**
+ * Shadow updating the cache entities in parallel.
+ *
+ * Use [add] to add a batch of scheduled updates.
+ *
+ * @param period - period between updating scheduled agents
+ * @param maxThreads - maximum threads for parallel updating cache entities
+ * @param logger - logger for debug information
+ */
+internal class Scheduler(
+    period: Long,
+    maxThreads: Int,
+    private val logger: Logger,
+) {
+    private val schedulerScope = CoroutineScope(Executors.newFixedThreadPool(maxThreads).asCoroutineDispatcher())
+    private val waitingUpdates = HashMap<String, ScheduledUpdate>()
+
+    init {
+        // start daemon timer with one periodical task to process updates every [delay] milliseconds
+        Timer(true).apply {
+            scheduleAtFixedRate(period, period) {
+                val batch = getBatch()
+                if (batch.isNotEmpty()) {
+                    logger.debug("process batch of ${batch.size} updates")
+                    process(batch)
+                }
+            }
+        }
+    }
+
+    /**
+     * Add a batch of scheduled updates
+     */
+    @Synchronized
+    fun add(updates: Collection<ScheduledUpdate>) {
+        updates.forEach { update ->
+            logger.debug("schedule updating {}", update)
+            // just replace the update if it already exists
+            waitingUpdates[update.name] = update
+        }
+    }
+
+    /**
+     * Process a batch of updates
+     *
+     * @param updates - batch of updates
+     */
+    private fun process(updates: Collection<ScheduledUpdate>) {
+        // Initially we have graph
+        //          0
+        //       1     1
+        //    2     2     2
+        // We can separate levels 0, 1, 2 and run its in parallel
+        // But, if we are at level 1, we must wait for the data to update at level 2, and so on
+        // sort updates by level in descending order
+        val levelSortedUpdates = updates.sortedByDescending { it.level }
+        // start from the highest level
+        var currentLevel = levelSortedUpdates.firstOrNull()?.level
+
+        class UpdatingJob(val job: Job, val update: ScheduledUpdate)
+
+        val levelJobs = mutableListOf<UpdatingJob>()
+
+        runBlocking {
+            levelSortedUpdates.forEach { update ->
+                val onFinish = update.onFinish
+
+                if (update.level != currentLevel) {
+                    // wait jobs at previous level
+                    levelJobs.forEach {
+                        logger.debug("wait coroutine job to update {}", it.update)
+                        it.job.join()
+                    }
+                    currentLevel = update.level
+                    levelJobs.clear()
+                }
+                logger.debug("create coroutine job to update {}", update)
+                val job = schedulerScope.launch {
+                    update.func()
+                    logger.debug("updated {}", update)
+                    onFinish?.invoke()
+                }
+                levelJobs.add(UpdatingJob(job, update))
+            }
+        }
+    }
+
+    /**
+     * Copies ALL values from [waitingUpdates] and clears it
+     *
+     * @return batch of updates
+     */
+    @Synchronized
+    private fun getBatch(): Collection<ScheduledUpdate> {
+        val batch = waitingUpdates.values.toList()
+        waitingUpdates.clear()
+        return batch
+    }
+}
+
+/**
+ * Scheduled update of the agent
+ *
+ * @param name unique name of the agent
+ * @param level level of the agent
+ * @param func function to update the agent
+ * @param onFinish optional callback that will be called after updating
+ */
+internal class ScheduledUpdate(
+    val name: String,
+    val level: Int,
+    val func: () -> Unit,
+    val onFinish: (() -> Unit)?,
+) {
+    /**
+     * Create a scheduled update from the [agent]
+     */
+    constructor(agent: CacheAgent<*>, onFinish: (() -> Unit)?) : this(
+        agent.name, agent.level, agent::update, onFinish
+    )
+
+    override fun toString() = "ScheduledUpdate(name=$name, level=$level)"
+}
